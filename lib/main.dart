b95f93533cc993712aa9ac4b5439053b4e361ab0
Chamika Ravinda <chamikaravinda@gmail.com>
2020-03-19 10:12:28 +0530

    separate widgets(extracting widgets)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './quoet.dart';
+import './quote_card.dart';
+
 void main() => runApp(MaterialApp(
   home: QuoteList(),
 ));
@@ -12,40 +14,12 @@ class QuoteList extends StatefulWidget {
 class _QuoteListState extends State<QuoteList> {
 
   List<Quote> quoests=[
-    Quote(text: 'Change your thoughts and you change your world',author: 'Unkon'),
-  Quote(text:'Those who realize their folly are not true fools',author: 'Unkon'),
-  Quote(text:'Life is too important to be taken seriously',author: 'Unkon'),
-  Quote(text:'It always seems impossible until it’s done',author: 'Unkon')
+    Quote(text: 'Change your thoughts and you change your world',author: 'Unknown'),
+  Quote(text:'Those who realize their folly are not true fools',author: 'Unknown'),
+  Quote(text:'Life is too important to be taken seriously',author: 'Unknown'),
+  Quote(text:'It always seems impossible until it’s done',author: 'Unknown')
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-                quote.text,
-                style:TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.grey[800]
-                )
-            ),
-            SizedBox(height: 6.0,),
-            Text(
-                quote.author,
-                style:TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.grey[600]
-                )
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,8 +30,9 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quoests.map((quoest) => quoteTemplate(quoest)).toList(),
+        children: quoests.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
 }
+
